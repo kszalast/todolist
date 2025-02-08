17f691962bc943431a78c71d9dad99c493c88936
kszalast <kszalast@egnyte.com>
2025-02-08 20:20:55 +0100

    refactored to maven module

--- src/main/java/com/egnyte/todolist/TaskRepository.java
+++
@@ -1,6 +0,0 @@
-package com.egnyte.todolist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
--- src/main/java/com/egnyte/todolist/TodoListRepository.java
+++
@@ -1,5 +0,0 @@
-package com.egnyte.todolist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoListRepository extends JpaRepository<TodoList, Long> {}
