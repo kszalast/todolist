fc3d50e66f004a08b2cbea515b43fc02a9565e7d
Michal Greda <mgreda@egnyte.com>
2025-02-24 20:27:23 +0100

    devops 5 separate service

--- exchange-rates/src/main/java/com/egnyte/exchangerates/ExchangeRatesController.java
+++ exchange-rates/src/main/java/com/egnyte/exchangerates/ExchangeRatesController.java
@@ -1,4 +1,4 @@
-package com.egnyte.todolist;
+package com.egnyte.exchangerates;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
