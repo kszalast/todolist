6372dc94ac58a89e4b9ca231c202718eb972e810
Michal Greda <mgreda@egnyte.com>
2025-03-18 09:54:41 +0100

    run sh

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
-mvn clean package
-cd monolit
-./1_build_docker_image.sh
-cd ../exchange-rates
-./7_build_docker_image.sh
-cd ../frontend
-./3_build_docker_image.sh
-cd ../haproxy
-./5_build_docker_image.sh
-cd ..
-docker compose up -d --force-recreate
+#mvn clean package
+#cd monolit
+#./1_build_docker_image.sh
+#cd ../exchange-rates
+#./7_build_docker_image.sh
+#cd ../frontend
+#./3_build_docker_image.sh
+#cd ../haproxy
+#./5_build_docker_image.sh
+#cd ..
+
+minikube start
+kubectl apply -f k8s/todo_list_deployment.yaml
+
+#kubectl get pods
