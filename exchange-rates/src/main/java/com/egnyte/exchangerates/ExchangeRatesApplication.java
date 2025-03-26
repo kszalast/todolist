b4c480723d38a577664f43619f6532d36daee0fc
wkowalewski <wkowalewski@vp.pl>
2025-03-26 08:57:37 +0100

    test

--- exchange-rates/src/main/java/com/egnyte/exchangerates/ExchangeRatesApplication.java
+++ exchange-rates/src/main/java/com/egnyte/exchangerates/ExchangeRatesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExchangeRatesApplication {
 
-    public static void main(String[] args) {
+    public staticd void main(String[] args) {
         SpringApplication.run(ExchangeRatesApplication.class, args);
     }
 
