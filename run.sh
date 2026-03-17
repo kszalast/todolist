42f770222b109b184150bc133de945990c7774ee
Michal Greda <mgreda@egnyte.com>
2026-03-17 09:22:20 +0100

    fix

--- run.sh
+++ run.sh
@@ -1,15 +1,20 @@
-#mvn clean package
-#cd monolit
-#./1_build_docker_image.sh
-#cd ../exchange-rates
-#./7_build_docker_image.sh
-#cd ../frontend
-#./3_build_docker_image.sh
-#cd ../haproxy
-#./5_build_docker_image.sh
-#cd ..
-
 minikube start
+minikube addons enable ingress
+eval $(minikube docker-env)
+
+mvn clean package
+cd monolit
+./1_build_docker_image.sh
+cd ../exchange-rates
+./7_build_docker_image.sh
+cd ../frontend
+./3_build_docker_image.sh
+cd ../haproxy
+./5_build_docker_image.sh
+cd ..
+
 kubectl apply -f k8s/todo_list_deployment.yaml
 
-#kubectl get pods
+eval $(minikube docker-env --unset)
+
+#kubectl get pods -n todo-app
